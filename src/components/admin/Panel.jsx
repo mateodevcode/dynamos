6de5118cc82fdf088035062ff74024d88962c0f8
Mateo Lizcano <149150878+mateodevcode@users.noreply.github.com>
2024-09-05 19:14:49 +0200

    Se configuro el panel de administrador

--- src/components/admin/Panel.jsx
+++ src/components/admin/Panel.jsx
@@ -1,6 +1,8 @@
 
-export const Panel = () => {
+const Panel = () => {
   return (
     <div className="bg-slate-300 h-[635px] w-full">Hola Administracion</div>
   );
 };
+
+export default Panel;
--- src/components/admin/PanelAdmin.jsx
+++ src/components/admin/PanelAdmin.jsx
@@ -1,6 +1,7 @@
-import Navbar from "@/components/admin/navbar";
-import { Panel } from "./Panel";
+import Navbar from "./Navbar";
 import Options from "./Options";
+import Panel from "./Panel";
+
 
 const PanelAdmin = () => {
   return (
