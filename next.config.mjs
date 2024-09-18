ced5d348bc6e95726602076819dc21b2ed5902b0
October29 <fpuerta1992@outlook.com>
2024-09-18 14:52:35 -0500

    picsum.photos, agregado a next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ["i.postimg.cc", "dummyimage.com", "i.blogs.es", "drive.google.com", "media.giphy.com", "visionyvalor.es"],
+        domains: ["i.postimg.cc", "dummyimage.com", "i.blogs.es", "drive.google.com", "media.giphy.com", "visionyvalor.es", "picsum.photos"],
     },
 };
 
