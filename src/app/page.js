036ac3cb977153d545ed466aaf984716741c488f
Rey <reydavidquiroz@gmail.com>
2024-09-18 13:32:34 -0500

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,21 @@
-// import Bienvenida from "@/components/bienvenida/Bienvenida";
-// import Eventos from "@/components/eventos/Eventos";
-// import Footer from "@/components/footer/Footer";
-// import Navbar from "@/components/navbar/Navbar";
-// import Podcasts from "@/components/podcasts/new/Podcasts";
-// import Team from "@/components/team/Team";
-// import Testimonios from "@/components/testimonios/Testimonios";
+import Bienvenida from "@/components/bienvenida/Bienvenida";
+import Eventos from "@/components/eventos/Eventos";
+import Footer from "@/components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
+import Podcasts from "@/components/podcasts/new/Podcasts";
+import Team from "@/components/team/Team";
+import Testimonios from "@/components/testimonios/Testimonios";
 
-// export default function Home() {
-//   return (
-//     <div>
-//       <Navbar />
-//       <Bienvenida />
-//       <Podcasts />
-//       <Eventos />
-//       <Testimonios />
-//       <Team />
-//       <Footer />
-//     </div>
-//   );
-// }
-import React from 'react'
-
-function page() {
+export default function Home() {
   return (
-    <div>page</div>
-  )
+    <div>
+      <Navbar />
+      <Bienvenida />
+      <Podcasts />
+      <Eventos />
+      <Testimonios />
+      <Team />
+      <Footer />
+    </div>
+  );
 }
-
-export default page
